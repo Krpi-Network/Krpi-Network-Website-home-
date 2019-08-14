dadcda2e57e4b2e1b67d28235bb38fab67c26bb1
Krpi-Network <KrpiNetwork@outlook.com>
2019-08-14 22:50:31 +0800

    edit menu

---
+++ menu-dir/AI/Flappy-Bird-AI/Preload.js
@@ -0,0 +1,10 @@
+function preload() {
+
+	birdSprite = loadImage("images/ChristmasBird.png");
+	birdSprite = loadImage("images/fatBird.png");
+	topPipeSprite = loadImage("images/full pipe top.png");
+	bottomPipeSprite = loadImage("images/full pipe bottom.png");
+	backgroundSprite = loadImage("images/background.png");
+	groundSprite = loadImage("images/groundPiece.png");
+
+}
